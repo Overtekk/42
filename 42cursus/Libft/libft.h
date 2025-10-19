bf0f28a23632202ce420d7f4a81d56e4c2f76f28
Overtek <theovertekgaming@gmail.com>
2025-10-19 17:20:40 +0000

    ft_putstr_fd

--- 42cursus/Libft/libft.h
+++ 42cursus/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 13:59:34 by roandrie          #+#    #+#             */
-/*   Updated: 2025/10/19 17:12:12 by roandrie         ###   ########.fr       */
+/*   Updated: 2025/10/19 17:20:20 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
