3c15224421ab0d2deff5426a6efe944eb8e4a0b7
Overtek <90642113+Overtekk@users.noreply.github.com>
2025-10-24 10:31:47 +0200

    add lstadd_back et lstsize

--- 42cursus/Libft/libft.h
+++ 42cursus/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 13:59:34 by roandrie          #+#    #+#             */
-/*   Updated: 2025/10/23 17:33:40 by roandrie         ###   ########.fr       */
+/*   Updated: 2025/10/24 10:29:49 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,8 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_putendl_fd(char *s, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
