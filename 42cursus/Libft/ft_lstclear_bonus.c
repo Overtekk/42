97428e85c791f7b45a21f2618de8f0d86697057c
Overtek <theovertekgaming@gmail.com>
2025-10-25 20:40:18 +0000

    add last exercices

--- 42cursus/Libft/ft_lstclear_bonus.c
+++ 42cursus/Libft/ft_lstclear_bonus.c
@@ -1,19 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/24 10:32:40 by roandrie          #+#    #+#             */
-/*   Updated: 2025/10/25 16:30:39 by roandrie         ###   ########.fr       */
+/*   Created: 2025/10/25 16:32:25 by roandrie          #+#    #+#             */
+/*   Updated: 2025/10/25 17:31:38 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	(*del)(lst->content);
-	free(lst);
+	t_list	*lst_cpy;
+	t_list	*temp;
+
+	lst_cpy = *lst;
+	while (lst_cpy != NULL)
+	{
+		temp = lst_cpy->next;
+		ft_lstdelone(lst_cpy, del);
+		lst_cpy = temp;
+	}
+	*lst = NULL;
 }
