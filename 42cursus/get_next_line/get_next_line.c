0859a1204888a11ac9b9a16f9335f9721f180e1c
Overtek <90642113+Overtekk@users.noreply.github.com>
2025-11-03 14:23:56 +0100

    updated GNL (not working yet)

--- 42cursus/get_next_line/get_next_line.c
+++ 42cursus/get_next_line/get_next_line.c
@@ -6,43 +6,82 @@
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 10:26:13 by roandrie          #+#    #+#             */
-/*   Updated: 2025/10/28 14:15:03 by roandrie         ###   ########.fr       */
+/*   Updated: 2025/11/03 14:21:43 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
+// char	*clear_buffer(char *large_buffer)
+// {
+// 	char	*new_buffer;
+// 	int		i;
 
-char	*get_next_line(int fd)
+// 	i = 0;
+// 	while (large_buffer[i] != '\n')
+// 	{
+// 		large_buffer++;
+// 		i++;
+// 	}
+// 	new_buffer = large_buffer;
+// 	free (large_buffer);
+// 	return (new_buffer);
+// }
+
+char	*fill_line(char *large_buffer)
 {
-	static char	*str;
-	char	buffer[BUFFER_SIZE + 1];
-	int		byte;
+	char	*new_line;
+	int		i;
 
-	byte = 1;
-	str = (char *)malloc(sizeof(char *) * BUFFER_SIZE);
-	if (!str)
-		return (NULL);
-	byte = read(fd, buffer, BUFFER_SIZE);
-	if (byte > 0)
-	{
-		str = buffer;
-		return (str);
-	}
-	return (NULL);
+	i = 0;
+	while (large_buffer[i] != '\n')
+		i++;
+	new_line = ft_substr(large_buffer, 0, i);
+	return (new_line);
 }
-/*
+
+char	*append_buffer(char *large_buffer, char *buffer_read)
+{
+	char	*temp;
+
+	temp = ft_strjoin(large_buffer, buffer_read);
+	free(large_buffer);
+	return (temp);
+}
+
+char	*read_line(int fd, char *large_buffer)
+{
+	char	*buffer_read;
+	int		byte_read;
+
+	buffer_read = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (buffer_read == NULL)
+		return (NULL);
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		byte_read = read(fd, buffer_read, BUFFER_SIZE);
+		if (byte_read <= -1)
+			return (free (buffer_read), NULL);
+		buffer_read[byte_read] = '\0';
+		large_buffer = append_buffer(large_buffer, buffer_read);
+		if (ft_strchr(large_buffer, '\n'))
+			break ;
+	}
+	free (buffer_read);
+	return (large_buffer);
+}
+
 char	*get_next_line(int fd)
 {
-	char	*str;
-	int		buff;
+	static char	*large_buffer;
+	char		*line;
 
-	str = malloc(8);
-	buff = 0;
-	buff = read(fd, str, 8);
-	if (buff > 0)
-		return (str);
-	return (NULL);
-}*/
-
+	if (large_buffer == NULL)
+		large_buffer = ft_strdup("");
+	if (ft_strchr(large_buffer, '\n') == NULL)
+		large_buffer = read_line(fd, large_buffer);
+	line = fill_line(large_buffer);
+	//large_buffer = clear_buffer(large_buffer);
+	return (line);
+}
