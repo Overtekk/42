4264fca416cef9254a7244082c7e11c6ba166d1d
Overtek <theovertekgaming@gmail.com>
2025-11-05 17:41:34 +0000

    change Makefile

--- 42cursus/ft_printf/ft_printf.h
+++ 42cursus/ft_printf/ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 15:27:55 by roandrie          #+#    #+#             */
-/*   Updated: 2025/11/03 10:25:49 by roandrie         ###   ########.fr       */
+/*   Updated: 2025/11/05 17:41:12 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "Libft/libft.h"
+# include "libft/libft.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
