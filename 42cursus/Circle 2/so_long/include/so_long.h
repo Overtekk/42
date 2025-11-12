1cb3bb5ce679b1969f70c64203953eab89e83da4
Romain Andrieux <roandrie@c1r1p5.42lehavre.fr>
2025-11-12 16:59:57 +0100

    commit 2

--- 42cursus/Circle 2/so_long/include/so_long.h
+++ 42cursus/Circle 2/so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 09:49:47 by roandrie          #+#    #+#             */
-/*   Updated: 2025/11/12 13:02:46 by roandrie         ###   ########.fr       */
+/*   Updated: 2025/11/12 16:58:31 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,28 @@
 # include "../libft/include/ft_printf.h"
 # include "../libft/include/get_next_line.h"
 # include "../libft/include/libft.h"
+
 //*MINILIBX HEADER*//
 # include "../minilibx-linux/mlx_int.h"
 # include "../minilibx-linux/mlx.h"
+
+//*LIBRARY*//
+#include "unistd.h"
+#include <fcntl.h>
+
+//*STRUCTURE*//
+typedef struct s_map
+{
+	int		x_map;
+	int		y_map;
+}	t_map;
+
+typedef struct s_game
+{
+	struct s_map	map;
+}	t_game;
+
+//*PROTYPE*//
+int	ft_print_error(char *err_msg);
 
 #endif
--- 42cursus/Circle 2/so_long/sources/ft_print_error.c
+++ 42cursus/Circle 2/so_long/sources/ft_print_error.c
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   set.c                                              :+:      :+:    :+:   */
+/*   ft_print_error.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/12 13:10:55 by roandrie          #+#    #+#             */
-/*   Updated: 2025/11/12 13:11:12 by roandrie         ###   ########.fr       */
+/*   Created: 2025/11/12 15:23:01 by roandrie          #+#    #+#             */
+/*   Updated: 2025/11/12 16:58:39 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-int	main(int argc, char *argv[])
+int	ft_print_error(char *err_msg)
 {
-
+	write(2, err_msg, ft_strlen(err_msg));
+	return (0);
 }
