2fa5ddef84f075c879a248e4af1bd4853f5a49c5
Romain Andrieux <roandrie@c1r1p5.42lehavre.fr>
2025-11-12 13:08:39 +0100

    first commit

---
+++ 42cursus/Circle 2/so_long/include/so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 09:49:47 by roandrie          #+#    #+#             */
+/*   Updated: 2025/11/12 13:02:46 by roandrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+//*LIBFT HEADER*//
+# include "../libft/include/ft_printf.h"
+# include "../libft/include/get_next_line.h"
+# include "../libft/include/libft.h"
+//*MINILIBX HEADER*//
+# include "../minilibx-linux/mlx_int.h"
+# include "../minilibx-linux/mlx.h"
+
+#endif
