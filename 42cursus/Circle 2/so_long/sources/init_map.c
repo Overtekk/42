b61bd969aa6e82b4f474eae21b404ac2bfd1125a
Overtek <90642113+Overtekk@users.noreply.github.com>
2025-11-13 22:13:56 +0100

    Update init_map.c

--- 42cursus/Circle 2/so_long/sources/init_map.c
+++ 42cursus/Circle 2/so_long/sources/init_map.c
@@ -12,12 +12,16 @@
 
 #include "../include/so_long.h"
 
-void	init_map(char *argv, t_game *game)
+int	init_map(char *argv, t_game *game)
 {
 	int	map_fd;
 
-	map_fd = open(argv[1], O_RDONLY);
+	map_fd = open(argv, O_RDONLY);
 	if (map_fd < 0)
+	{
 		return (ft_print_error("Error\nOpening map failed.\n"));
+		return (1);
+	}
 	close (map_fd);
+	return (0);
 }
