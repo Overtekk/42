93d81ea8a559c7d3c97ad4ca39001199b93f632d
Overtek <90642113+Overtekk@users.noreply.github.com>
2025-11-13 19:58:40 +0100

    Update monitoring.sh

--- 42cursus/Circle 1/born2beroot/monitoring.sh
+++ 42cursus/Circle 1/born2beroot/monitoring.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# ARCH
+# ARCHITECTURE
 arch=$(uname -a)
 
 # CPU PHYSICAL
-cpuf=$(grep "physical id" /proc/cpuinfo | wc -l)
-
-# CPU VIRTUAL
-cpuv=$(grep "processor" /proc/cpuinfo | wc -l)
+cpuf=$(grep -c "physical id" /proc/cpuinfo)
+cpuv=$(grep -c "processor" /proc/cpuinfo)
 
 # RAM
 ram_total=$(free --mega | awk '$1 == "Mem:" {print $2}')
@@ -20,18 +18,19 @@ disk_use=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} END {pri
 disk_percent=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} {disk_t+= $2} END {printf("%d"), disk_u/disk_t*100}')
 
 # CPU LOAD
-cpul=$(vmstat 1 2 | tail -1 | awk '{printf $15}')
-cpu_op=$(expr 100 - $cpul)
-cpu_fin=$(printf "%.1f" $cpu_op)
+cpu_fin=$(vmstat 1 2 | tail -1 | awk '{printf "%.1f", 100 - $15}')
 
 # LAST BOOT
 lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
 
 # LVM USE
-lvmu=$(if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then echo yes; else echo no; fi)
-
+if lsblk | grep -q "lvm"; then
+  lvmu="yes"
+else
+  lvmu="no"
+fi
 # TCP CONNEXIONS
-tcpc=$(ss -ta | grep ESTAB | wc -l)
+tcpc=$(ss -ta | grep -c ESTAB)
 
 # USER LOG
 ulog=$(users | wc -w)
@@ -41,7 +40,7 @@ ip=$(hostname -I)
 mac=$(ip link | grep "link/ether" | awk '{print $2}')
 
 # SUDO
-cmnd=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+cmnd=$(journalctl _COMM=sudo | grep -c COMMAND)
 
 wall "
 Architecture: $arch
@@ -50,7 +49,7 @@ vCPU: $cpuv
 Memory Usage: $ram_use/${ram_total}MB ($ram_percent%)
 Disk Usage: $disk_use/${disk_total} ($disk_percent%)
 CPU Load: $cpu_fin%
-Last Loot: $lb
+Last Boot: $lb
 LVM Use: $lvmu
 Connections TCP: $tcpc ESTABLISHED
 User Log: $ulog
