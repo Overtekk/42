896d5ec978aed88f0a25e557e69fc6502e751c2e
Overtek <90642113+Overtekk@users.noreply.github.com>
2025-11-18 19:25:17 +0100

    commentary to help me understand

--- 42cursus/Circle 2/so_long/sources/check_invalid_path.c
+++ 42cursus/Circle 2/so_long/sources/check_invalid_path.c
@@ -12,23 +12,45 @@
 
 #include "../include/so_long.h"
 
-static	void	flood_fill(char **tab, t_game *game)
+/*
+typedef struct s_path_check
 {
-	if (game->map.y > 0 || game->map.x > 0)
-		return ;
-	tab[y][x] = '1';
+    int    collectibles_found;
+    int    exit_reached;
+}   t_path_check;
+*/
+
+static	void	flood_fill(char **tab, t_game *game, int y, int x, t_path_check *status)
+{
+	/*
+	typedef struct s_path_check
+	{
+    int    collectibles_found;
+    int    exit_reached; // 0 (false) ou 1 (true)
+	}   t_path_check;
+	*/
+	// arret si tab[y][x] = 1 ou deja visite
+	// transformer les case visitee = V
+	// compter les collectibles + exit (status->collectibles_found = status->collectibles_found + 1)
+	// recursive en appelant les 4 voisins en bas
+	// y-1, x
+	// y+1, x
+	// y, x-1
+	// ym x+1
 }
 
 int	is_valid_path(t_game *game)
 {
 	char	**tab;
-	int		y;
-	int		size;
+	// t_path_check    status;
+	// status.collectibles_found = 0;
+	// status.exit_reached = 0;
 
-	size = game->map.y;
-	tab = malloc(sizeof(char *) * (size + 1));
-	tab = game->map.grid;
+	tab = malloc(sizeof(char *) * (game->map.y));
+	// boucle pour copier (while puis strdup)
 	tab[y++] = NULL;
-	flood_fill(tab, game);
+	flood_fill(tab, game->map.player.y, game->map.player.x, &status);
+	// regarder si collectibles + exit = ok
+	// free la copie du tableau
 	return (0);
 }
