c1323ea0996bdac023f810630d81de9e3b988a18
Romain Andrieux <roandrie@c1r3p1.42lehavre.fr>
2025-12-11 13:23:09 +0100

    module01

--- 42cursus/Circle 2/new_cursus/Python Module 01/ex1/ft_garden_data.py
+++ 42cursus/Circle 2/new_cursus/Python Module 01/ex1/ft_garden_data.py
@@ -1,17 +1,22 @@
 def ft_garden_data():
     class Plant:
-        def ___init___(self, name, height, days):
+        def __init__(self, name, height, days):
             self.name = name
             self.height = height
             self.days = days
 
-    plant_one = Plant('Rose', 25, 30)
-    plant_two = Plant('Sunflower', 80, 45)
-    plant_three = Plant('Cactus', 15, 120)
+        def display_info(self):
+            print(f"{self.name}: {self.height}cm, {self.days} days old")
+
+    plant_one = Plant("Rose", 25, 30)
+    plant_two = Plant("Sunflower", 80, 45)
+    plant_three = Plant("Cactus", 15, 120)
 
     print("=== Garden Plant Registry ===")
-    print(f"{plant_one}")
+    plant_one.display_info()
+    plant_two.display_info()
+    plant_three.display_info()
 
 
-if __name__ == "__main__":
-    ft_garden_data()
+# if __name__ == "__main__":
+#     ft_garden_data()
