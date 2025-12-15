e816d9a538842439a667625da20999de94852304
Romain Andrieux <roandrie@c1r3p1.42lehavre.fr>
2025-12-15 13:16:12 +0100

    change variables to private

--- 42cursus/Circle 2/new_cursus/Python Module 01/ex4/ft_garden_security.py
+++ 42cursus/Circle 2/new_cursus/Python Module 01/ex4/ft_garden_security.py
@@ -1,49 +1,49 @@
 class SecurePlant:
-    """Init class"""
+    """Init class with private variables"""
     def __init__(self, name, height, age):
-        self.name = name
-        self.height = height
-        self.age = age
+        self.__name = name
+        self.__height = height
+        self.__age = age
 
     """Display information about plant"""
     def get_info(self):
-        print(f"{self.name} ({self.height}cm, {self.age} days)")
+        print(f"{self.__name} ({self.__height}cm, {self.__age} days)")
 
     """Set new height ONLY if it's valid"""
     def set_height(self, new_height):
         if (new_height < 0):
             print("Invalid operation attempted: height", end=" ")
-            print(f"{self.height} [REJECTED]")
+            print(f"{self.__height} [REJECTED]")
         else:
-            self.height = new_height
-            print(f"Height updated: {self.height}cm [OK]")
+            self.__height = new_height
+            print(f"Height updated: {self.__height}cm [OK]")
 
     """Set new age ONLY if it's valid"""
     def set_age(self, new_age):
         if (new_age < 0):
             print("Invalid operation attempted: age", end=" ")
-            print(f"{self.age} [REJECTED]")
+            print(f"{self.__age} [REJECTED]")
         else:
-            self.age = new_age
-            print(f"Age updated: {self.age} days [OK]")
+            self.__age = new_age
+            print(f"Age updated: {self.__age} days [OK]")
 
     """Check if starting height value is valid"""
     def get_height(self):
-        if (self.height < 0):
+        if (self.__height < 0):
             print("Security: Negative height rejected")
         else:
-            print(f"Height updated: {self.height}cm [OK]")
+            print(f"Height updated: {self.__height}cm [OK]")
 
     """Check if starting age value is valid"""
     def get_age(self):
-        if (self.age < 0):
+        if (self.__age < 0):
             print("Security: Negative age rejected")
         else:
-            print(f"Age updated: {self.age} days [OK]\n")
+            print(f"Age updated: {self.__age} days [OK]\n")
 
     """Create plant and check if informations are valid"""
     def check_security(self, plant):
-        print(f"Plant created: {self.name}")
+        print(f"Plant created: {self.__name}")
         plant.get_height()
         plant.get_age()
 
