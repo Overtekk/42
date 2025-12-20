cd95064e222b9cbcfd89b269a27f86a7868f93ca
Romain Andrieux <roandrie@c1r3p2.42lehavre.fr>
2025-12-20 10:34:57 +0100

    finish

--- 42cursus/Circle 2/new_cursus/Python Module 03/ex1/ft_score_analytics.py
+++ 42cursus/Circle 2/new_cursus/Python Module 03/ex1/ft_score_analytics.py
@@ -2,16 +2,27 @@ import sys
 
 
 def ft_score_analytics() -> None:
-    """
-    Accepted arguments: a list of INT. It will be the player scores.
-    Then, organize scores and make some calcules.
+    """Processes and displays scores provided by the user.
+
+    === Arguments ===
+        - No arguments requiered
+
+    === Returns ===
+        - None: This function only prints to stdout
+
+    This function only accept numbers provided by the user. Store
+    them in a list and organize scores. Calculate the number of players, total
+    score, average score, high score, low score and score range.
     """
 
     print("=== Player Score Analytics ===")
 
     # === Check if at least 1 argument is given ===
 
-    if len(sys.argv) == 1:
+    try:
+        if len(sys.argv) == 1:
+            raise ValueError
+    except ValueError:
         print("No scores provided. Usage: python3 ft_score_analytics.py "
               "<score1> <score2> ...")
         return None
@@ -26,18 +37,32 @@ def ft_score_analytics() -> None:
         print("Error: All arguments must be valid integers")
         return None
 
-    # === Show the score ===
+    # === Score calculation ===
 
     i = 0
+    score_range = max(score_list) - min(score_list)
+    arg_count = len(score_list)
 
     print("Scores processed: [", end="")
     for score in score_list:
-        if i == score_list[-1]:
-            print(f"{score}")
+        if i == arg_count - 1:
+            print(f"{score}]")
         else:
-            print(f"{score}", end=",")
+            print(f"{score}", end=", ")
         i += 1
-    print("]")
+
+    print(f"Total players: {arg_count}")
+
+    print(f"Total score: {sum(score_list)}")
+
+    average_score = sum(score_list) / arg_count
+    print(f"Average score: {average_score}")
+
+    print(f"Hight score: {max(score_list)}")
+
+    print(f"Low score: {min(score_list)}")
+
+    print(f"Score range: {score_range}")
 
 
 if __name__ == "__main__":
