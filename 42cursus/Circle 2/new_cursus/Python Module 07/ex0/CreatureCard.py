8a4fbb2262c9defbe2ae1b20dc085d95ae60ccf3
Romain Andrieux <roandrie@c1r3p1.42lehavre.fr>
2026-01-08 14:48:54 +0100

    add docstring

--- 42cursus/Circle 2/new_cursus/Python Module 07/ex0/CreatureCard.py
+++ 42cursus/Circle 2/new_cursus/Python Module 07/ex0/CreatureCard.py
@@ -2,9 +2,19 @@ from .Card import Card
 
 
 class CreatureCard(Card):
+    """Class for Card of type Creature."""
 
     def __init__(self, name: str, cost: int, rarity: str,
                  attack: int, health: int) -> None:
+        """Init special attributes specific to a CreatureCard.
+
+        === Args ===
+            - name (str): The name of the card.
+            - cost (int): Cost of the card (positive only).
+            - rarity (str): Rarity of the card.
+            - attack (int): Attack value of the card.
+            - health (int): Health of the card.
+        """
         if health <= 0:
             raise ValueError("ERROR: Health must be positive.")
         if attack < 0:
@@ -17,6 +27,11 @@ class CreatureCard(Card):
         self.type = "Creature"
 
     def get_card_info(self) -> dict:
+        """Get informations about a specific card.
+
+        === Return ===
+            - dict: Informations about the card.
+        """
         return ({
             "name": self.name,
             "cost": self.cost,
@@ -27,6 +42,14 @@ class CreatureCard(Card):
         })
 
     def play(self, game_state: dict) -> dict:
+        """Play a card only if it can be played.
+
+        === Args ===
+            - game_state (dict): Informations about the state of the game:
+
+        === Return ===
+            - dict: Informations about the card played.
+        """
         if self.is_playable(game_state.get("mana_left")):
             return ({
                 "card_played": self.name,
@@ -40,6 +63,14 @@ class CreatureCard(Card):
             })
 
     def attack_target(self, target: Card) -> dict:
+        """Attack a target with a card
+
+        === Args ===
+            - target (Card): The card to attack.
+
+        === Return ===
+            - dict: Informations about the battle.
+        """
         if target is None:
             raise ValueError("Target is missing")
         return ({
