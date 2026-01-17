0ce93c05d14060d8e18697e573e0dd6a7cfd9ccd
roandrie <roandrie@student.42lehavre.fr>
2026-01-17 16:21:52 +0100

    wip

--- 42cursus/Circle 2/new_cursus/Python Module 10/ex0/lambda_spells.py
+++ 42cursus/Circle 2/new_cursus/Python Module 10/ex0/lambda_spells.py
@@ -4,7 +4,7 @@ def artifact_sorter(artifacts: list[dict]) -> list[dict]:
 
 
 def power_filter(mages: list[dict], min_power: int) -> list[dict]:
-    result = filter(lambda item: ())
+    result = filter(lambda item: (item['power'] >= min_power), mages)
     return result
 
 
@@ -26,10 +26,13 @@ def main() -> None:
         {'name': 'Magic Keyboard', 'power': 52, 'type': 'coder'}
     ]
     sorted = artifact_sorter(artifact_list)
-    for item in sorted:
-        print(f"{item['name']} ({item['power']} power)", end=" comes before ")
+    msg = [f"'{item['name']}' ({item['power']} power)" for item in sorted]
+    msg = " come before ".join(msg)
+    print(msg)
 
-    print("\n\nTesting power filter...")
+    print("\n\nTesting power filter...", end="")
+    min_power = 42
+    print(f"(>= {min_power})")
     mage_list = [
         {'name': 'Gérard le Gris', 'power': 66, 'element': 'fire'},
         {'name': 'Remy l\'Ignoble', 'power': 42, 'element': 'mud'},
@@ -37,10 +40,16 @@ def main() -> None:
         {'name': 'Monoco himself', 'power': 2, 'element': 'feets'},
         {'name': 'Doggo the Dog', 'power': 12, 'element': 'bone'}
     ]
-    filtered = power_filter(mage_list, 42)
-    for item in filtered:
-        print(f"{item['name']}", end=" - ")
+    filtered = power_filter(mage_list, min_power)
+    msg = [f"'{item['name']}' ({item['power']} power)"
+               for item in filtered]
+    msg = " - ".join(msg)
+    print(msg)
 
+    print("\nTesting spell transformer...")
+    spell_list = ["Fireball", "Nuclear", "Kiss", "Heal", "Shield"]
+    transform = spell_transformer(spell_list)
+    print(transform)
 
 if __name__ == "__main__":
     main()
